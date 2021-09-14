9e7f5e519dc09af7502c9c125a0a5e994365bf35
zhanghuijiang <zhanghuijiang@aobi.com>
2021-09-14 14:47:07 +0800

    feat：【工具类】新增函数：扫描所有预制体，移除丢失引用的MonoBehaviour脚本

---
+++ Assets/Scripts/CSharp/Editor/Utils/UFGameObjectUtils.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace CSharp.Editor.Utils
+{
+    public class UFGameObjectUtils
+    {
+        private const string UtilsPrefix = "Tools/EditorUtils";
+
+        [MenuItem(UtilsPrefix + "/ClearMissingScript")]
+        public static void ClearMissingMonoBehaviours()
+        {
+            var guids = AssetDatabase.FindAssets("t:prefab");
+            foreach (var guid in guids)
+            {
+                var path = AssetDatabase.GUIDToAssetPath(guid);
+                var go = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+                if (go)
+                {
+                    if (GameObjectUtility.GetMonoBehavioursWithMissingScriptCount(go) > 0)
+                        GameObjectUtility.RemoveMonoBehavioursWithMissingScript(go);
+                }
+            }
+            AssetDatabase.Refresh();
+        }
+    }
+}
